019549e3cba4d432a48f3e58035a8f59268d5c96
Jamel Seagraves <jseagra5@ford.com>
2015-06-03 08:29:22 -0700

    created project and trip unit test template

---
+++ Carpooler/app/src/main/java/com/carpooler/Trip.java
@@ -0,0 +1,7 @@
+package com.carpooler;
+
+/**
+ * Created by jcsax on 6/2/15.
+ */
+public class Trip {
+}
---
+++ Carpooler/app/src/test/java/com/carpooler/TripTest.java
@@ -0,0 +1,47 @@
+package com.carpooler;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by jcsax on 6/2/15.
+ */
+public class TripTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testAddCarpoolUser() {
+
+    }
+
+    public void testRemoveCarpoolUser() {
+
+    }
+
+    public void testPickupCarpoolUser() {
+
+    }
+
+    public void testDropoffCarpoolUser() {
+
+    }
+
+    public void testSkipNoShow() {
+
+    }
+
+    public void testRequestPickup() {
+
+    }
+
+    public void testSplitFuelCost() {
+
+    }
+
+}
