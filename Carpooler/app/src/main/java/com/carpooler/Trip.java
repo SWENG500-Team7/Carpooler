488df85aa1e0404624293d4d9678dec46dcbc9e0
Jamel Seagraves <jseagra5@ford.com>
2015-06-06 16:34:45 -0700

    built Trip class

--- Carpooler/app/src/main/java/com/carpooler/Trip.java
+++ Carpooler/app/src/main/java/com/carpooler/Trip.java
@@ -7,31 +7,40 @@ import java.util.List;
  */
 public class Trip {
 
-    public void addCarpoolUser(CarpoolUser user) {
+    List<CarpoolUser> users;
 
+    public void addCarpoolUser(CarpoolUser user) {
+        users.add(user);
+        user.setStatus(CarpoolUserStatus.CONFIRMED);
     }
 
     public void removeCarpoolUser(CarpoolUser user) {
-
+        users.remove(user);
+        user.setStatus(CarpoolUserStatus.UNLISTED);
     }
 
     public void pickupCarpoolUser(CarpoolUser user) {
-
+        user.setStatus(CarpoolUserStatus.PICKED_UP);
     }
 
     public void dropoffCarpoolUser(CarpoolUser user) {
-
+        user.setStatus(CarpoolUserStatus.DROPPED_OFF);
     }
 
     public void skipNoShow(CarpoolUser user) {
-
+        user.setStatus(CarpoolUserStatus.NO_SHOW);
     }
 
     public void requestPickup(CarpoolUser user) {
-
+        user.setStatus(CarpoolUserStatus.PENDING);
     }
 
     public double splitFuelCost(double cost) {
+        double fuel_split = cost/users.size();
+        for(CarpoolUser user : users) {
+            user.setPaymentAmount(fuel_split);
+        }
+        // TODO: Need to round fuel_split to two decimal places
         return fuel_split;
     }
 
