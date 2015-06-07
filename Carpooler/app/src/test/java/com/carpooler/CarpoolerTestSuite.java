e040d87ed55769445a43c1af45f62e608bf53592
raymond <raymond.munian@gmail.com>
2015-06-07 18:49:57 -0400

    Added TripTest

--- Carpooler/app/src/test/java/com/carpooler/CarpoolerTestSuite.java
+++ Carpooler/app/src/test/java/com/carpooler/CarpoolerTestSuite.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({CarpoolHostTest.class,CarpoolUserTest.class,PaymentServiceTest.class})
+@Suite.SuiteClasses({CarpoolHostTest.class,CarpoolUserTest.class,PaymentServiceTest.class,TripTest.class})
 public class CarpoolerTestSuite {
 
 }
