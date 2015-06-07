ea6b0d1bae04e53bae797525005257fcdc18c744
rym002 <raymond.munian@gmail.com>
2015-06-06 23:21:04 -0400

    Initial creation

---
+++ Carpooler/app/src/main/java/com/carpooler/Address.java
@@ -0,0 +1,34 @@
+package com.carpooler;
+
+/**
+ * Created by raymond on 6/6/15.
+ */
+public class Address {
+    private GeoPoint location;
+    private String  streetAddress;
+    private String zip;
+
+    public GeoPoint getLocation() {
+        return location;
+    }
+
+    public void setLocation(GeoPoint location) {
+        this.location = location;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+}
---
+++ Carpooler/app/src/main/java/com/carpooler/GeoPoint.java
@@ -0,0 +1,25 @@
+package com.carpooler;
+
+/**
+ * Created by raymond on 6/6/15.
+ */
+public class GeoPoint {
+    private double longititude;
+    private double latitude;
+
+    public double getLongititude() {
+        return longititude;
+    }
+
+    public void setLongititude(double longititude) {
+        this.longititude = longititude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+}
