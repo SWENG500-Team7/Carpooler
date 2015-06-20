d7c09b8a58102c6a39a65bdfcf177a268f50cecc
vgroove <kevinwhadaway@gmail.com>
2015-06-20 14:52:06 -0400

    Added VehicleRestServiceTest

---
+++ Carpooler/app/src/androidTest/java/com/carpooler/test/VehicleRestServiceTest.java
@@ -0,0 +1,34 @@
+package com.carpooler.test;
+
+import android.test.AndroidTestCase;
+import android.test.ServiceTestCase;
+
+import com.carpooler.dao.VehicleRestService;
+
+import junit.framework.TestCase;
+
+import java.util.Arrays;
+
+/**
+ * Created by Kevin on 6/20/2015.
+ */
+public class VehicleRestServiceTest extends AndroidTestCase {
+
+    public void testGetYears() {
+        String[] yearsArray = VehicleRestService.getYears();
+        assertTrue(yearsArray.length > 0);
+        assertTrue(Arrays.asList(yearsArray).contains("2006"));
+    }
+
+    public void testGetMakes() {
+        String[] makesArray = VehicleRestService.getMakes("2015");
+        assertTrue(makesArray.length > 0);
+        assertTrue(Arrays.asList(makesArray).contains("Dodge"));
+    }
+
+    public void testGetModels() {
+        String[] modelsArray = VehicleRestService.getModels("2006", "Dodge");
+        assertTrue(modelsArray.length > 0);
+        assertTrue(Arrays.asList(modelsArray).contains("Charger"));
+    }
+}
